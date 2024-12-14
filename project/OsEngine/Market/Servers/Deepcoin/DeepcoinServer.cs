fba8db056804df2881ee81b4b3e60069c0cd4b92
Ярослав <yaroslavponomarev.pro@gmail.com>
2024-12-14 22:09:13 +0400

    Добавил необходимые регионы. Открывается окно с параметрами для подключения.

--- project/OsEngine/Market/Servers/Deepcoin/DeepcoinServer.cs
+++ project/OsEngine/Market/Servers/Deepcoin/DeepcoinServer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using OsEngine.Entity;
+using OsEngine.Language;
 using OsEngine.Logging;
 using OsEngine.Market.Servers.Entity;
 
@@ -10,29 +11,140 @@ namespace OsEngine.Market.Servers.Deepcoin
     {
         public DeepcoinServer()
         {
+            DeepcoinServerRealization realization = new DeepcoinServerRealization();
+            ServerRealization = realization;
             
+            CreateParameterString(OsLocalization.Market.ServerParamPublicKey, "");
+            CreateParameterPassword(OsLocalization.Market.ServerParamSecretKey, "");
+            CreateParameterPassword(OsLocalization.Market.ServerParamPassphrase, "");
         }
     }
 
     public class DeepcoinServerRealization : IServerRealization
     {
-        public ServerType ServerType { get; }
-        public ServerConnectStatus ServerStatus { get; set; }
-        public DateTime ServerTime { get; set; }
-        public List<IServerParameter> ServerParameters { get; set; }
-        
-        public void Connect()
+        #region 1 Constructor, Status, Connection
+
+        // В данном регионе располагается конструктор реализации сервера. Реализация методов Connect / Disconnect. Тип сервера и т.д.
+        // То, что нужно для старта работы сервера.
+
+        public DeepcoinServerRealization()
         {
-            throw new NotImplementedException();
+            ServerStatus = ServerConnectStatus.Disconnect;
+            
+            
         }
 
+        public void Connect()
+        {
+            _publicKey = ((ServerParameterString)ServerParameters[0]).Value;
+            _secretKey = ((ServerParameterPassword)ServerParameters[1]).Value;
+            _passphrase = ((ServerParameterPassword)ServerParameters[2]).Value;
+            
+            ServerStatus = ServerConnectStatus.Connect;
+        }
+        
         public void Dispose()
         {
-            throw new NotImplementedException();
+            ServerStatus = ServerConnectStatus.Disconnect;
         }
 
         public event Action ConnectEvent;
+        
         public event Action DisconnectEvent;
+
+        #endregion
+
+        #region 2 Properties
+
+        // В данном регионе располагаются настройки коннектора.
+        
+        private string _baseUrl = "https://api.deepcoin.com";
+        
+        private string _publicKey;
+        
+        private string _secretKey;
+        
+        private string _passphrase;
+        
+        public ServerType ServerType => ServerType.Deepcoin;
+        
+        public ServerConnectStatus ServerStatus { get; set; }
+        
+        public DateTime ServerTime { get; set; }
+        
+        public List<IServerParameter> ServerParameters { get; set; }
+
+        #endregion
+
+        #region 3 Securities
+
+        // Запрос доступных для подключения бумаг у подключения. Плюс всё, что с этим связано.
+
+        #endregion
+
+        #region 4 Portfolios
+
+        // Запрос доступных портфелей у подключения. Плюс всё, что с этим связано.
+
+        #endregion
+
+        #region 5 Data
+
+        // Запросы данных по свечкам и трейдам. Плюс всё, что с этим связано.        
+
+        #endregion
+
+        #region 6 WebSocket creation
+
+        // Создание вебсокет соединения. Плюс всё, что с этим связано.
+
+        #endregion
+
+        #region 7 WebSocket events
+
+        // Обработка входящих сообщений от вёбсокета. И что важно в данном конкретном случае, Closed и Opened методы обязательно должны находиться здесь, а не только приём сообщений. Ибо без них стабильно наладить работу вёбсокета не выйдет.
+
+        #endregion
+
+        #region 8 WebSocket check alive
+
+        // Проверка вёбсокета на работоспособность путём отправки ему пингов.
+
+        #endregion
+
+        #region 9 Security subscrible
+
+        // Подписка на бумагу. В данном случае это один метод, но бывает их тут и три. Поэтому отдельный регион.
+        
+        // С обязательным контролем скорости и кол-ву запросов к методу Subscrible через rateGate. Иначе будут проблемы, когда пользователи начнут подписываться на 400 источников.
+
+        #endregion
+
+        #region 10 WebSocket parsing the messages
+
+        // Разбор сообщений от сокета и отправка их наверх.
+
+        #endregion
+
+        #region 11 Trade
+
+        // Регион, посвящённый торговле. Выставление ордеров, отзыв и т.д.
+
+        #endregion
+
+        #region 12 Queries
+
+        // Место расположение HTTP запросов.
+
+        #endregion
+
+        #region 13 Log
+
+        // Логирование.
+
+        #endregion
+        
+        
         
         public void GetSecurities()
         {
